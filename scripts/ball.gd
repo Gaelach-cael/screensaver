a4092401f5d2c784efe5ddc3eba00c39a5f5bdb9
Gaelach-cael <cael.viala@gedu.sd73.bc.ca>
2025-02-10 13:27:25 -0800

    woah

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
 func _ready():
-    #set movement speed
-    velocity = Vector2(150, 150)
+	#set movement speed
+	velocity = Vector2(150, 150)
 
 func _physics_process(delta: float):
 
-    var collision = move_and_collide(velocity * delta)
+	var collision = move_and_collide(velocity * delta)
 
 
-    if collision:
-        var normal = collision.get_normal()
+	if collision:
+		var normal = collision.get_normal()
 
-        velocity = velocity.bounce(normal)
+		velocity = velocity.bounce(normal)
