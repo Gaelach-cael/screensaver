dd8157028c6e6983052c82e86ebb0f50a4866e11
Gaelach-cael <cael.viala@gedu.sd73.bc.ca>
2025-02-10 13:19:08 -0800

    oorah

---
+++ scripts/ball.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+func _ready():
+    #set movement speed
+    velocity = Vector2(150, 150)
+
+func _physics_process(delta: float):
+
+    var collision = move_and_collide(velocity * delta)
+
+
+    if collision:
+        var normal = collision.get_normal()
+
+        velocity = velocity.bounce(normal)
