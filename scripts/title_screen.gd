76302e43cf9e5af0135d3d3fb858154c969c8f07
Gaelach-cael <cael.viala@gedu.sd73.bc.ca>
2025-02-11 10:35:59 -0800

    Update title_screen.gd

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,10 +1,7 @@
 extends Control
 
-
 func _on_square_pressed():
 	get_tree().change_scene_to_file("res://scenes/square.tscn")
-
-
 
 func _on_circle_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
